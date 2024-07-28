1f598290b05a604394d2c1f4777ea596c336653f
grian <grianguy32@gmail.com>
2024-07-28 21:02:22 +0300

    add sql via sqldelight & postgre

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,21 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    id("app.cash.sqldelight") version "2.0.2"
 }
 
 group = "me.grian"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    google()
     mavenCentral()
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.ktor:ktor-server-netty:3.0.0-beta-2")
+    implementation("app.cash.sqldelight:jdbc-driver:2.0.2")
+    implementation("com.zaxxer:HikariCP:5.1.0")
 }
 
 tasks.test {
@@ -19,4 +23,13 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+sqldelight {
+    databases {
+        create("Database") {
+            packageName.set("me.grian")
+            dialect("app.cash.sqldelight:postgresql-dialect:2.0.2")
+        }
+    }
 }
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,10 +1,27 @@
+import app.cash.sqldelight.db.SqlDriver
+import app.cash.sqldelight.driver.jdbc.asJdbcDriver
+import com.zaxxer.hikari.HikariDataSource
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import me.grian.Database
+import me.grian.data.SellQueries
 import java.io.File
 
+
 fun main() {
+    val dataSource: HikariDataSource = HikariDataSource()
+
+    // TODO: move to config file
+
+    dataSource.jdbcUrl = "jdbc:postgresql://localhost:5432/grinvacc"
+    dataSource.username = "admin"
+    dataSource.password = "12345"
+
+    val driver: SqlDriver = dataSource.asJdbcDriver()
+    val database = Database(driver)
+
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
