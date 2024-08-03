c742a1b679da24cc0807bcb71ce49ceb14839f1f
grian <grianguy32@gmail.com>
2024-08-03 15:40:41 +0300

    sell and buy add/remove from stock

--- src/main/kotlin/data/Ext.kt
+++ src/main/kotlin/data/Ext.kt
@@ -3,12 +3,18 @@ package data
 import me.grian.Database
 import me.grian.Items
 
-// TODO: sell and buy also remove the amount sold/ add the amount bought to the specified item's stock.
+fun SellData.addToDb(database : Database) {
+    database.sellQueries.insert(itemId, amountSold, pricePerItem)
+    val stockBeforeSell = database.itemsQueries.selectWithId(itemId).executeAsOne().current_stock
+    database.itemsQueries.updateStock(stockBeforeSell - amountSold, itemId)
+}
 
-fun SellData.addToDb(database : Database) = database.sellQueries.insert(itemId, amountSold, pricePerItem)
 
-
-fun BuyData.addToDb(database : Database) = database.buyQueries.insert(itemId, amountBought, pricePerItem)
+fun BuyData.addToDb(database : Database) {
+    database.buyQueries.insert(itemId, amountBought, pricePerItem)
+    val stockBeforeBuy = database.itemsQueries.selectWithId(itemId).executeAsOne().current_stock
+    database.itemsQueries.updateStock(stockBeforeBuy + amountBought, itemId)
+}
 
 
 fun ItemData.addToDb(database : Database) = database.itemsQueries.insert(itemName, important)
