ca08401405cfc86103b95670d299b030b7aa5b8c
grian <grianguy32@gmail.com>
2024-08-07 00:45:19 +0300

    remove useless spaces

--- src/main/kotlin/data/Ext.kt
+++ src/main/kotlin/data/Ext.kt
@@ -11,16 +11,13 @@ fun SellData.addToDb(database : Database) {
     database.itemsQueries.updateStock(stockBeforeSell - amountSold, itemId)
 }
 
-
 fun BuyData.addToDb(database : Database) {
     database.buyQueries.insert(itemId, amountBought, pricePerItem)
     val stockBeforeBuy = database.itemsQueries.selectWithId(itemId).executeAsOne().current_stock
     database.itemsQueries.updateStock(stockBeforeBuy + amountBought, itemId)
 }
 
-
 fun ItemData.addToDb(database : Database) = database.itemsQueries.insert(itemName, important)
-
 
 fun Items.toItemData() = ItemData(id, current_stock, item_name, important)
 
